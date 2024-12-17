b07b37b6346abeb66466cf6954aeb551db6f746b
rfzn <rafizansamian@gmail.com>
2024-12-17 14:26:00 +0800

    Update syp.py

--- syp.py
+++ syp.py
@@ -39,33 +39,6 @@ def create_performance_trend_chart(year_data):
     )
     return fig
 
-# Data for the Kelas Tanah table
-def get_soil_class_table():
-    data = {
-        "Kelas Tanah": ["Kelas 1 (Sangat sesuai)"] * 2 + ["Kelas 2 (Sesuai)"] * 28 + ["Kelas 3 (Kurang sesuai)"] * 30,
-        "Jenis Tanah": ["SELANGOR", "KANGKONG", "BRIAH", "TELONG", "SEGAMAT", "KUANTAN", "BENTA", "SK. MAS", 
-                        "KATONG", "SABAK", "YONG PENG", "TOM YONG", "KEMUNING", "JERANGAU", "JEMBANG", "JELAI", 
-                        "CHAMP", "MUSANG", "SEMPAKA", "KAMPONG KOLAM", "MASSA", "LENGKAWI", "NENASI", "BATANG MERBAU", 
-                        "LIMBAT", "TEBOK", "KALLA", "COLLUVIUM", "BUKIT", "ORGANIC ALLUVIUM", "LOCAL ALLUVIUM", 
-                        "BUNGOR", "CHEROK", "SEMUPURNA/IMD", "BATANG", "BESAI", "RAU", "TAPAH", "RASAU", "BUNGOR", 
-                        "ROMPIN", "RUDUA", "MUNCHONG/L", "BUNGOR/S", "DURIAN", "CHENIAN/S", "DUTALAN", "BATU LAPAN", 
-                        "HARRADIL", "MERAPOH/L", "MT. HAIL", "MALACCA", "HARRADS", "GAJAH MATI", "MERAPOH", "HARAD/ACAD", 
-                        "KEDAH", "TAY", "SEREMBAN", "KANTIS", "KALI BUKIT", "DURIAN ALAM", "SEMPORNA/S", "BATU/M", 
-                        "PAGOH", "ORGANIC CLAY MUCK", "SANDY COLLUVIUM", "SRANTI", "KUALA BERANG", "BUKIT TUKU", 
-                        "KAMP. KUBUR", "ULU TIRAM", "JABIL", "SEDIRANG", "MARANG", "TAPAH", "BINA", "KUALA BRANG", 
-                        "PEAT/D"],
-        "Kod": ["SLR", "KGR", "BRH", "TLG", "SGT", "KTN", "BNT", "SMS", "KTG", "SBK", "YPG", "TYN", "KMG", "JRN", "JMB", 
-                "JLC", "CPG", "MUS", "SPA", "KKL", "MSI", "LKI", "NBI", "MRB", "LBT", "TBK", "KLL", "COL", "BKT", "ORA", 
-                "LRA", "BGR", "CRK", "SPAMD", "BTG", "BSH", "RAU", "TGH", "RAS", "RGR", "RPN", "RDA", "MUNL", "BRS", 
-                "DUR", "CHNS", "DTL", "BNL", "HRDL", "MRPL", "MTH", "MLC", "HRDS", "GMH", "MRP", "HRA", "KDH", "TAY", 
-                "SRB", "KNT", "KBUK", "DAL", "SMP", "BTM", "PGH", "OCM", "SCL", "SRA", "KBG", "BKTK", "KPR", "ULT", 
-                "JBL", "SDR", "MRG", "TGH", "BNA", "KLG/KBG", "PET/D"],
-        "Kumpulan": [1, 1, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 
-                     4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 
-                     5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6]
-    }
-    return pd.DataFrame(data)
-
 def get_topography_table():
     data = {
         "TOPOGRAFI": ["Beralun Lemah", "Beralun Sederhana", "Berbukit"],
@@ -102,8 +75,8 @@ def main():
     
     # Display Soil Class Table
     st.subheader("Maklumat Topografi")
-    soil_df = get_topography_table()
-    st.dataframe(soil_df)
+    topo_df = get_topography_table()
+    st.dataframe(topo_df)
 
     st.markdown("### Developed by Rafizan Samian - FELDA Strategy & Transformation Department")
 
